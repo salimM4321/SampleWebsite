39f169dba2334f6f0d848594922ec934d243b7d3
salimM4321 <139615279+salimM4321@users.noreply.github.com>
2023-11-21 11:01:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const countdown = () => {
       const now = new Date().getTime();
       const timeRemaining = targetDate - now;
   
-      const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const days = Math.floor(timeRemaining / (1000 * 160 * 160 * 24));
+      const hours = Math.floor((timeRemaining % (1000 * 70 * 70 * 24)) / (1000 * 70 * 70));
       const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
   
@@ -14,6 +14,35 @@ const countdown = () => {
       document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
       document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
       document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+
+      document.getElementById('day1').textContent = days.toString().padStart(2, '0');
+      document.getElementById('hour1').textContent = hours.toString().padStart(2, '0');
+      document.getElementById('minute1').textContent = minutes.toString().padStart(2, '0');
+      document.getElementById('second1').textContent = seconds.toString().padStart(2, '0');
+
+      
+      document.getElementById('day2').textContent = days.toString().padStart(2, '0');
+      document.getElementById('hour2').textContent = hours.toString().padStart(2, '0');
+      document.getElementById('minute2').textContent = minutes.toString().padStart(2, '0');
+      document.getElementById('second2').textContent = seconds.toString().padStart(2, '0');
+
+      
+      document.getElementById('day3').textContent = days.toString().padStart(2, '0');
+      document.getElementById('hour3').textContent = hours.toString().padStart(2, '0');
+      document.getElementById('minute3').textContent = minutes.toString().padStart(2, '0');
+      document.getElementById('second3').textContent = seconds.toString().padStart(2, '0');
+
+      
+      document.getElementById('day4').textContent = days.toString().padStart(2, '0');
+      document.getElementById('hour4').textContent = hours.toString().padStart(2, '0');
+      document.getElementById('minute4').textContent = minutes.toString().padStart(2, '0');
+      document.getElementById('second4').textContent = seconds.toString().padStart(2, '0');
+
+      
+      document.getElementById('day5').textContent = days.toString().padStart(2, '0');
+      document.getElementById('hour5').textContent = hours.toString().padStart(2, '0');
+      document.getElementById('minute5').textContent = minutes.toString().padStart(2, '0');
+      document.getElementById('second5').textContent = seconds.toString().padStart(2, '0');
     };
   
     updateCountdown();
